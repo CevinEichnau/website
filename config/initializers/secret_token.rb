63fa19438d6cffadf5a2dabe4072d18d64f37530
Cevin Eichnau <Cevin@cevins-mac-mini.fritz.box>
2013-01-31 11:37:11 +0100

    dd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = 'ec915998913d293d6c1cb6513e2b32903b91ecbf5d0254b11c2c11dcaad88880b5b46f88f55a7f347485d1b925ff4a3cca11da29d7fa3ceb7673f276e5d92d1f'
