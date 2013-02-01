bbffdc949dec76f46cd09b7aa9fd096ad5833980
Cevin Eichnau <cevin@empuxa.com>
2013-02-01 16:48:08 +0100

    d

---
+++ app/assets/javascripts/facebook.js
@@ -0,0 +1,31 @@
+window.fbAsyncInit = function() {
+
+  FB.init({
+    appId      : '457219220998113', 
+    channelUrl : '//eichnau.com/contact.html', 
+    status     : true, 
+    cookie     : true, 
+    xfbml      : true  
+  });
+};
+
+
+(function(d, debug){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+   ref.parentNode.insertBefore(js, ref);
+ }(document, /*debug*/ false));
+
+function facebook_send_message(to) {
+  FB.ui({
+      app_id:'457219220998113',
+      method: 'send',
+      name: "sdfds jj jjjsdj j j ",
+      link: 'http://www.eichnau.com',
+      to:to,
+      description:'sdf sdf sfddsfdd s d  fsf s '
+
+  });
+}
