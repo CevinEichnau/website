1de335cf652ba212c18d7a4279b4ccea25053ded
Cevin Eichnau <cevin@empuxa.com>
2013-02-08 21:18:17 +0100

    ich mag popel

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ set :user, "user46572014"
 
 
 # password to login to the target server
-set :password, "ich mag popel"
+set :password, "ich mag popel 1wqdqwd"
 
 
 ## Application name and repository
