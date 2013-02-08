a28324110a44f1e5c12a4bc563abf8b6090c64e7
Cevin Eichnau <cevin@empuxa.com>
2013-02-08 09:41:50 +0100

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,5 @@
 class UsersController < ApplicationController
-   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessor :password
-  # attr_accessible :title, :body
-  #before_filter :authenticate_user!
 
   def index
     @user = User.find(params[:id])
