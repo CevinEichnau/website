8a55007e95a03da1af5333d15b1094a23956e481
Cevin Eichnau <cevin@empuxa.com>
2013-04-08 13:46:24 +0200

    swagger die zweite

--- app/api/website_api.rb
+++ app/api/website_api.rb
@@ -3,10 +3,13 @@ module WebsiteAPI
   class Root < Grape::API
 
     mount WebsiteAPI::V1::Users
+
+    if !Rails.env.production?  
     
-    add_swagger_documentation :api_version => "v1",
-                              :base_path => "http://#{Rails.configuration.default_host}/api",
-                              :hide_documentation_path => true
+      add_swagger_documentation :api_version => "v1",
+                                :base_path => "http://#{Rails.configuration.default_host}/api",
+                                :hide_documentation_path => true
+    end                          
                             
     rescue_from :all do |exception|
       send_exception_email(exception)
